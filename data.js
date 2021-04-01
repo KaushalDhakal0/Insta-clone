30c5650f2236b062049e631c50c09be46f8891b0
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2021-04-01 22:42:04 +0545

    Add files via upload

---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    url:
+      "https://www.tiktok.com/@suzeet_.ff/video/6929134679983459586?is_copy_url=0&is_from_webapp=v1&sender_device=pc&sender_web_id=6929834214879725058",
+    channel: "@kaushal",
+    song: "I am alive",
+    likes: "6456",
+    messages: "20",
+    description: "This is dummy description",
+    shares: "250",
+  },
+  {
+    url:
+      "https://www.tiktok.com/@suzeet_.ff/video/6929134679983459586?is_copy_url=0&is_from_webapp=v1&sender_device=pc&sender_web_id=6929834214879725058",
+    channel: "@kaushal",
+    song: "I am alive",
+    likes: "6456",
+    messages: "209",
+    description: "This is dummy description",
+    shares: "250",
+  },
+];
+
+export default data;
---
+++ server.js
@@ -0,0 +1,73 @@
+import express from "express";
+import mongoose from "mongoose";
+// import Data from "./data.js";
+import dbModel from "./dbModel.js";
+
+// app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+// middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"),
+    res.setHeader("Access-Control-Allow-Headers", "*"),
+    next();
+});
+
+// Db Config
+
+const connecction_url =
+  "mongodb+srv://admin:kaushal@db2341@cluster0.rewlg.mongodb.net/tiktok?retryWrites=true&w=majority";
+
+mongoose.connect(connecction_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+//api endpoints
+
+app.get("/", (req, res) => {
+  res.status(200).send("If u see this then backend is completed.");
+});
+
+app.get("/v1/posts", (req, res) => {
+  dbModel.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      console.log(data);
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.get("/v2/posts", (req, res) => {
+  // retrieves data from collections of mongoDb.
+  dbModel.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      console.log(data);
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/v2/posts", (req, res) => {
+  //It lets video document to be added in the collection in mongodb.
+  console.log(req.body);
+  const dbVideos = req.body;
+
+  dbModel.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+//listen on port
+app.listen(port, () => console.log(`listening on port : ${port}`));
